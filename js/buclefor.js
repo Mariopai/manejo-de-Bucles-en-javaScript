507965465129eb136f1da10ead5b0536fa6dce54
Mariopai <mario_wwe_mario@hotmail.com>
2022-03-29 23:15:03 -0600

    manejo de Bucles en javaScript

---
+++ js/buclefor.js
@@ -0,0 +1,9 @@
+for(var vuelta=0;vuelta <=1000; vuelta++){
+    if(vuelta>=2){
+        document.write(" "+vuelta+" piezas de computadora");   
+    }else{
+        document.write(" "+vuelta+" piezas de computadora");   
+    }
+    
+
+}
---
+++ js/buclewhile.js
@@ -0,0 +1,8 @@
+var conteo=0;
+
+while(conteo<=200){
+    document.write(" "+conteo+" carros ");
+   
+    conteo++;
+}
+
